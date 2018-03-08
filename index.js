be1861de733f0b0d0c3634aab311f9027e464faa
EdsonLI <edsonisele@outlook.com>
2018-03-07 22:27:04 -0300

    Preparing OAuth20 Google API

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
+/* console.developers.google.com */
+/* clientID 501241993264-u8tae954pgu9f968gjgpfufa4i903a94.apps.googleusercontent.com */
+/* clientSecret BCD3tvfd3xM1FS9VgKxqHJWT */
+passport.use(new GoogleStrategy());
+
+/*app.get("/", (req, res) => {
   res.send({ hey: "Good Luck!" });
-});
+});*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
