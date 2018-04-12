b6e0e7051c4dab9fd66e82772048f9bea2867802
EdsonLI <edsonisele@outlook.com>
2018-04-12 01:41:11 -0300

    Recording googleId with mongoose_mongodb

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
 
-const app = express();
+
+mongoose.connect(keys.mongoURI);
+
+const app = express(); /* app object - IMPORTANT */
 
 require('./routes/authRoutes')(app);
-
-/*app.get("/", (req, res) => {
-  res.send({ hey: "Good Luck!" });
-});*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-/*
- * console.developers.google.com
- * clientID
- * clientSecret
- */
+const User = mongoose.model('users');
+
 passport.use(
   new GoogleStrategy(
     {
@@ -15,9 +13,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ', accessToken);
-      console.log('refreshToken: ', refreshToken);
-      console.log('profile: ', profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
